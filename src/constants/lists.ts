38c5e424c0beb47958a78d40b995294a4bbb7cd7
crypzoh <crypzoh@protonmail.com>
2022-05-07 20:34:08 +0200

    Disable community list by default (#82)

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -28,4 +28,4 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DIFFUSION_LIST, DIFFUSION_COMMUNITY_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DIFFUSION_LIST]
