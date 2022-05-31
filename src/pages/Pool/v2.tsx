ed14de2c2de1f050807706e9d3ecd6ae5f0414b8
crypzoh <crypzoh@protonmail.com>
2022-05-31 10:15:51 +0200

    Change labels for fees to 0.25 (#102)

--- src/pages/Pool/v2.tsx
+++ src/pages/Pool/v2.tsx
@@ -126,7 +126,7 @@ export default function Pool() {
         <SwapPoolTabs active={'pool'} />
         <InfoCard
           title="Liquidity provider rewards"
-          description={`Liquidity providers earn a 0.3% fee on all trades proportional to their share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.`}
+          description={`Liquidity providers earn a 0.25% fee on all trades proportional to their share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.`}
         />
 
         <AutoColumn gap="lg" justify="center">
