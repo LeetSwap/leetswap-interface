d320f941b3104b5f0249e9b1b288ed201bd2f585
Prophet-42 <98233106+Prophet-42@users.noreply.github.com>
2022-11-10 00:31:40 +0100

    feat: change token and contract addresses

--- src/constants/native-token.ts
+++ src/constants/native-token.ts
@@ -5,17 +5,14 @@ import invariant from 'tiny-invariant'
 import { WETH9_ADDRESS } from './addresses'
 
 export const WEVMOS = {
-  // Mainly for unit tests
-  1: new Token(1, WETH9_ADDRESS[4], 18, 'WEVMOS', 'Wrapped Evmos'),
-  [ChainId.MAINNET]: new Token(ChainId.MAINNET, WETH9_ADDRESS[ChainId.MAINNET], 18, 'WEVMOS', 'Wrapped Evmos'),
-  [ChainId.TESTNET]: new Token(ChainId.TESTNET, WETH9_ADDRESS[ChainId.TESTNET], 18, 'WEVMOS', 'Wrapped Evmos'),
-  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, WETH9_ADDRESS[ChainId.RINKEBY], 18, 'WEVMOS', 'Wrapped Evmos'),
+  [ChainId.MAINNET]: new Token(ChainId.MAINNET, WETH9_ADDRESS[ChainId.MAINNET], 18, 'wCANTO', 'Wrapped Canto'),
+  [ChainId.TESTNET]: new Token(ChainId.TESTNET, WETH9_ADDRESS[ChainId.TESTNET], 18, 'wCANTO', 'Wrapped Canto'),
 }
 export const WETH9 = WEVMOS
 
 export class Evmos extends NativeCurrency {
   protected constructor(chainId: number) {
-    super(chainId, 18, 'EVMOS', 'Evmos')
+    super(chainId, 18, 'CANTO', 'Canto')
   }
 
   public get wrapped(): Token {
