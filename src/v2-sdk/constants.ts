856e4af844f3dd5f49e92546f3529946382e5052
Prophet-42 <98233106+Prophet-42@users.noreply.github.com>
2023-02-17 20:09:02 +0100

    feat: route through both cantodex and leetswap v2

--- src/v2-sdk/constants.ts
+++ src/v2-sdk/constants.ts
@@ -1,6 +1,7 @@
 import JSBI from 'jsbi'
 
-export const INIT_CODE_HASH = '0x97653931c50be3c0550346c96798d2d21ba0ebddcbc1a6debaa0669b70bb5735'
+export const INIT_CODE_HASH = '0x6af919ca83de85b5b311c668befb2e211ed1d5cc2785630849d3a2ec234f9042'
+export const CANTODEX_INIT_CODE_HASH = '0x97653931c50be3c0550346c96798d2d21ba0ebddcbc1a6debaa0669b70bb5735'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
