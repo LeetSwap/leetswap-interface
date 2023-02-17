769340b8fb7c7ed6a6a7de88ef3b774dbcaeb98f
Prophet-42 <98233106+Prophet-42@users.noreply.github.com>
2023-02-18 00:09:32 +0100

    feat: add staking

--- src/pages/Stake/StakingPage.tsx
+++ src/pages/Stake/StakingPage.tsx
@@ -39,8 +39,8 @@ const Heading = styled.div`
 // const APYRow = styled(RowBetween)`
 //   background: ${({ theme }) =>
 //     `linear-gradient(90deg, ${theme.darkTransparent} 0%, ${theme.secondary1_30} 50%, ${theme.darkTransparent} 100%);`};
-//   border: 1px solid rgba(4, 76, 26, 0.7);
-//   box-shadow: 0 0 5px rgba(74, 222, 128, 0.1), 0 0 7px rgba(74, 222, 128, 0.3);
+//   border: 1px solid rgba(66, 26, 4, 0.7);
+//   box-shadow: 0 0 5px rgba(245, 158, 11, 0.1), 0 0 7px rgba(245, 158, 11, 0.3);
 //   border-radius: 8px;
 //   padding: 2% 5%;
 //   width: 50%;
@@ -51,17 +51,19 @@ export function StakingPage() {
   return (
     <>
       <PageWrapper>
-        <Tux src={TuxImg} />
+        {/* <Tux src={TuxImg} /> */}
         <Heading>
-          <PotionIcon width={60} height={60} />
-          <TYPE.largeHeader style={{ margin: 0 }}>Boost your yield by staking LEET for xLEET</TYPE.largeHeader>
+          {/* <PotionIcon width={60} height={60} /> */}
+          <TYPE.largeHeader style={{ margin: 0 }}>Earn rewards by staking LEET for xLEET</TYPE.largeHeader>
         </Heading>
         <AutoColumn gap="lg" justify="center">
           <InfoCard
             title="Staking rewards"
-            description={`For every swap on the exchange, half of 0.05% of the 0.30% swap fees are distributed as LEET proportional to your share of the staking pool.  Additionally, daily Diffusion holder rewards from inflation are deposited into this pool. When your LEET is staked you receive xLEET.
+            description={`The sell tax on our token is distributed as LEET proportional to your share of the staking pool. When your LEET is staked you receive xLEET.
 
-          ${`\n`} Your xLEET is continuously compounding, when you unstake you will receive all the originally deposited LEET and any additional from fees and daily Diffusion holders rewards`}
+          ${`\n`} Your xLEET is continuously compounding, when you unstake you will receive all the originally deposited LEET plus any additional tokens accrued from fees.
+
+          ${`\n`} NOTE: The APR shown is based on the fees generated in the last 24 hours and may vary over time.`}
           />
           <StakingBalance />
         </AutoColumn>
