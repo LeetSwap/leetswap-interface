fd72dcc7ed6e92bd8be1ddf3760308bd36d23f2a
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-03-30 06:14:47 +0200

    feat: temporarily use eth price for calculations

--- src/hooks/useUSDCPrice.ts
+++ src/hooks/useUSDCPrice.ts
@@ -1,6 +1,6 @@
 import { Currency, CurrencyAmount, Price, Token } from '@uniswap/sdk-core'
 import { useMemo } from 'react'
-import { USDC } from '../constants/tokens'
+import { ETH, USDC, WETH } from '../constants/tokens'
 import { useV2TradeExactOut } from './useV2Trade'
 import { useActiveWeb3React } from './web3'
 import { ChainId } from 'constants/chains'
@@ -11,7 +11,8 @@ import JSBI from 'jsbi'
 // const usdcCurrencyAmount = CurrencyAmount.fromRawAmount(USDC[ChainId.MAINNET], 100_000e6)
 // @TODO: change back, but to bootstrap we dont have enought liquidity
 const usdcCurrencyAmount = {
-  [ChainId.MAINNET]: CurrencyAmount.fromRawAmount(USDC[ChainId.MAINNET], 100e6),
+  // [ChainId.MAINNET]: CurrencyAmount.fromRawAmount(USDC[ChainId.MAINNET], 100e6),
+  [ChainId.MAINNET]: CurrencyAmount.fromRawAmount(WETH[ChainId.MAINNET], 1e5),
   [ChainId.TESTNET]: CurrencyAmount.fromRawAmount(USDC[ChainId.TESTNET], 100e18),
 }
 /**
@@ -34,8 +35,11 @@ export default function useUSDCPrice(currency?: Currency): Price<Currency, Token
       return undefined
     }
 
-    if (currency && _usdcCurrencyAmount.currency.equals(currency)) {
-      return new Price(currency, USDC[chainId], JSBI.BigInt(1), JSBI.BigInt(1))
+    const wethPrice = new Price(currency, WETH[chainId], JSBI.BigInt(1e6), JSBI.BigInt(1800*1e6))
+
+    if (currency && _usdcCurrencyAmount.currency.equals(currency) || currency.equals(ETH)) {
+      // return new Price(currency, USDC[chainId], JSBI.BigInt(1), JSBI.BigInt(1))
+      return wethPrice
     }
 
     // return some fake price data for non-mainnet
@@ -51,8 +55,9 @@ export default function useUSDCPrice(currency?: Currency): Price<Currency, Token
     // use v2 price if available, v3 as fallback
     if (v2USDCTrade) {
       const { numerator, denominator } = v2USDCTrade.route.midPrice
-      const adjustmentFactor = _usdcCurrencyAmount.currency.equals(currency) ? JSBI.BigInt(10**12) : JSBI.BigInt(1)
-      return new Price(currency, USDC[chainId], JSBI.multiply(denominator, adjustmentFactor), numerator)
+      // const adjustmentFactor = _usdcCurrencyAmount.currency.equals(currency) ? JSBI.BigInt(10**12) : JSBI.BigInt(1)
+      // return new Price(currency, USDC[chainId], JSBI.multiply(denominator, adjustmentFactor), numerator)
+      return new Price(currency, WETH[ChainId.MAINNET], JSBI.multiply(denominator, wethPrice.denominator), JSBI.multiply(numerator, wethPrice.numerator))
     }
 
     return undefined
