83ad8a4bdbea1e8edd0ef61869e706a53f499f6f
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-04-26 16:17:31 +0200

    fix: update uniswap tokenlist schema

--- src/utils/getTokenList.ts
+++ src/utils/getTokenList.ts
@@ -1,5 +1,5 @@
 import { TokenList } from '@uniswap/token-lists'
-import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
+import { tokenlistSchema as schema } from '@uniswap/sdk-core'
 import Ajv from 'ajv'
 import contenthashToUri from './contenthashToUri'
 import { parseENSAddress } from './parseENSAddress'
