4ddfafbd7d9afc7631f85be43666297d98eb8164
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-07-24 01:09:50 +0200

    fix: wrong busd decimals

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -10,7 +10,7 @@ export { WETH, Eth }
 export const ETH = Eth.onChain(ChainId.MAINNET)
 
 export const USDC = {
-  [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0x7d43AABC515C356145049227CeE54B608342c0ad', 6, 'BUSD', 'BUSD (via Celer.Network)'),
+  [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0x7d43AABC515C356145049227CeE54B608342c0ad', 18, 'BUSD', 'BUSD (via Celer.Network)'),
   [ChainId.TESTNET]: new Token(ChainId.TESTNET, TESTNET.mockUSDC, 18, 'MUSDC', 'Mock USDC'),
 }
 
