4c09e0051670c897ca159f9098cb32d1879f2ea6
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-08-04 01:44:42 +0200

    feat: add warning banner

--- src/components/WarningBanner/NomadWarningBanner.tsx
+++ src/components/WarningBanner/NomadWarningBanner.tsx
@@ -7,16 +7,17 @@ export function NomadWarningBanner({ style }: { style?: any }) {
   return (
     <InfoCard
       style={style}
-      title="Nomad Assets on Eth"
+      title="WARNING: OUR DEX IS COMPROMISED"
       description={
         <ol>
           <li>
-            The Nomad Bridge contract on Ethereum mainnet was exploited. We are working with the Nomad and Eth teams
-            and will keep you updated.
+            The LeetSwap contracts were exploited. All of the pools are unsafe and liquidity can be stolen.
           </li>
           <li>
-            The USD prices on LeetSwap are derived from madUSDC. Given the exploit Nomad assets might not have their
-            expected value.
+            If you have still some tokens you can try to sell them for ETH and salvage what you can.
+          </li>
+          <li>
+            DO NOT BUY ANY TOKEN OR YOU WILL LOSE YOUR FUNDS.
           </li>
         </ol>
       }
